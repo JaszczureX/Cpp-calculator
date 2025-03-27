dfa8c568c0a526819699267dbd7789a2a86dac3c
Ryszard W. <116756583+JaszczureX@users.noreply.github.com>
2025-03-27 21:27:08 +0100

    fix

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
 int user_choice;
 char exit_char;
+
 //=============Counting Functions===========//
 int sqr(int b) {
-    b = b * b;
-    return b;
+    return b * b;
 }
+
 long int factorial(int x) {
     long int result = 1;
     for (int i = 1; i <= x; i++) {
@@ -17,15 +18,16 @@ long int factorial(int x) {
     }
     return result;
 }
-float quadric_tritomial(float a, float b, float c, float& x1, float& x2) {
-    float delta = sqr(b) - 4 * a * c;
+
+bool quadratic_trinomial(float a, float b, float c, float& x1, float& x2) {
+    float delta = b * b - 4 * a * c;
     if (delta > 0) {
-        x1 = (-b - sqrt(delta) / 2 * a);
-        x2 = (-b + sqrt(delta) / 2 * a);
+        x1 = (-b - sqrt(delta)) / (2 * a);
+        x2 = (-b + sqrt(delta)) / (2 * a);
         return true;
     }
     else if (delta == 0) {
-        x1 = x2 = (-b / 2 * a);
+        x1 = x2 = -b / (2 * a);
         return true;
     }
     else {
@@ -34,31 +36,71 @@ float quadric_tritomial(float a, float b, float c, float& x1, float& x2) {
 }
 
 long int fib(int x) {
-    if (x < 3) {
-        return 1;
+    if (x <= 0) return 0;
+    if (x == 1) return 1;
+    long int a = 0, b = 1, temp;
+    for (int i = 2; i <= x; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+float median(int* tab, int table_length) {
+    sort(tab, tab + table_length);
+    if (table_length % 2 != 0) {
+        return tab[table_length / 2];
     }
     else {
-        return fib(x - 2) + fib(x - 1);
+        return (tab[table_length / 2 - 1] + tab[table_length / 2]) / 2.0f;
     }
 }
-//=============Counting Functions===========//
+
+float arithmeticMean(int* tab, int table_length) {
+    float sum = 0;
+    for (int i = 0; i < table_length; i++) {
+        sum += tab[i];
+    }
+    return sum / table_length;
+}
+
+int power(int base, int exponent) {
+    return pow(base, exponent);
+}
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
 //=============Void Functions===========//
 void interface() {
     cout << "1. Factorial\n";
     cout << "2. Quadratic trinomial\n";
     cout << "3. Fibonacci sequence\n";
     cout << "4. Median and arithmetic mean\n";
-    cout << "5. \n";
-    cout << "Choose actions: ";
+    cout << "5. Power function\n";
+    cout << "6. Greatest Common Divisor (GCD)\n";
+    cout << "7. Least Common Multiple (LCM)\n";
+    cout << "Choose action: ";
 }
 
-static void calculator() {
-    do
-    {
+void calculator() {
+    do {
         system("cls");
         interface();
         cin >> user_choice;
-        if (user_choice > 4 || user_choice < 1) {
+
+        if (user_choice < 1 || user_choice > 7) {
             system("cls");
             cout << "You made a mistake in typing action!";
         }
@@ -71,17 +113,13 @@ static void calculator() {
         }
         else if (user_choice == 2) {
             system("cls");
-            cout << "Provide coefficients a, b, c.\n";
-            float a, b, c;
-            float x1{ 0 }, x2{ 0 };
+            cout << "Provide coefficients a, b, c:\n";
+            float a, b, c, x1, x2;
             cin >> a >> b >> c;
-            if (quadric_tritomial(a, b, c, x1, x2)) {
-                system("cls");
-                cout << "x1 = " << x1;
-                cout << "\nx2 = " << x2;
-            }
-            else {
-                cout << "Quadric tritomial has no solutions";
+            if (quadratic_trinomial(a, b, c, x1, x2)) {
+                cout << "x1 = " << x1 << "\nx2 = " << x2;
+            } else {
+                cout << "Quadratic trinomial has no real solutions";
             }
         }
         else if (user_choice == 3) {
@@ -89,35 +127,53 @@ static void calculator() {
             cout << "Enter the term of the sequence: ";
             int x;
             cin >> x;
-            cout << "\nterm nr: " << x << " is " << fib(x);
+            cout << "\nTerm nr " << x << " is " << fib(x);
         }
         else if (user_choice == 4) {
             system("cls");
             cout << "Enter the table length: ";
-            int table_length = 0;
+            int table_length;
             cin >> table_length;
+            
             int* tab = new int[table_length];
-            float sum = 0;
+            cout << "Enter " << table_length << " numbers: ";
             for (int i = 0; i < table_length; i++) {
                 cin >> tab[i];
-                sum += tab[i];
             }
-            sort(tab, tab + table_length);
-            cout << "\nArithmetic mean: " << sum / table_length;
-            if (table_length % 2 != 0)
-            {
-                cout << "\nMedian :" << tab[table_length / 2];
-            }
-            else {
-                cout << "\nMedian :" << (tab[table_length / 2 - 1] + tab[table_length / 2]) / 2;
-            }
+
+            cout << "\nMedian: " << median(tab, table_length);
+            cout << "\nArithmetic mean: " << arithmeticMean(tab, table_length);
+            
+            delete[] tab;
         }
-        cout << "\nWould you like to exit the application?[Y/N]";
+        else if (user_choice == 5) {
+            system("cls");
+            int base, exponent;
+            cout << "Enter base and exponent: ";
+            cin >> base >> exponent;
+            cout << "Result: " << power(base, exponent);
+        }
+        else if (user_choice == 6) {
+            system("cls");
+            int a, b;
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+            cout << "GCD: " << gcd(a, b);
+        }
+        else if (user_choice == 7) {
+            system("cls");
+            int a, b;
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+            cout << "LCM: " << lcm(a, b);
+        }
+        
+        cout << "\nWould you like to exit the application? [Y/N] ";
         cin >> exit_char;
     } while (exit_char == 'n' || exit_char == 'N');
 }
-//=============Void Functions===========//
-int main()
-{
+
+int main() {
     calculator();
+    return 0;
 }
